de31de5eba78fac6c3291f0ed78bd441bf2c9a04
Roman Trifonov <roman1trifonoff@gmail.com>
2024-07-03 17:40:35 +0200

    clean up the code

--- main.js
+++ main.js
@@ -6,29 +6,26 @@ document.addEventListener('dblclick', (event) => {
 
     const videoRect = videoPlayer.getBoundingClientRect();
     const clickX = event.clientX - videoRect.left;
-    const clickY = event.clientY - videoRect.top;
-    if (clickX < 0 || clickX > videoRect.width || clickY < 0 || clickY > videoRect.height) {
+    if (clickX < 0 || clickX > videoRect.width) {
         return;
     }
 
-    const tapAreaPercentage = 0.2; // 20% area on each side of the video
-    const tapAreaWidth = videoRect.width * tapAreaPercentage;
+    const clickY = event.clientY - videoRect.top;
+    if (clickY < 0 || clickY > videoRect.height) {
+        return;
+    }
+
+    const tapAreaWidth = videoRect.width * 0.2; // 20% area on each side of the video
     if (clickX <= tapAreaWidth) {
-        // rewind if tap is in the left area
-        replaceEvent(event, new KeyboardEvent('keydown', { keyCode: 74,  code: 'KeyJ' }))
+        event.stopImmediatePropagation();
+        event.preventDefault();
+        document.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 74,  code: 'KeyJ' }));
         return;
     }
 
     if (clickX >= videoRect.width - tapAreaWidth) {
-        // fast-forward if tap is in the right area
-        replaceEvent(event, new KeyboardEvent('keydown', { keyCode: 76,  code: 'KeyL' }))
+        event.stopImmediatePropagation();
+        event.preventDefault();
+        document.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 76,  code: 'KeyL' }));
     }
 }, true); // Use capture phase to ensure we intercept the double-click early
-
-function replaceEvent(originalEvent, newEvent) {
-    // Prevent fullscreen toggle on double-tap
-    originalEvent.stopImmediatePropagation();
-    originalEvent.preventDefault();
-
-    document.dispatchEvent(newEvent);
-}
